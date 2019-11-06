e7afcf2091abc5c248d26deab83ac95f3569a182
fahim-abdullah <fahimabdullahh@gmail.com>
2019-11-07 07:47:13 +0800

    user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_204352) do
+ActiveRecord::Schema.define(version: 2019_11_06_234448) do
 
   create_table "orders", force: :cascade do |t|
     t.string "fullname"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2019_11_06_204352) do
     t.string "price"
     t.string "old_price"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
