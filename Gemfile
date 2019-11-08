5d4016cdbeb3f78bf5151bbfd861d0b43ebd609c
Mahfuzur Rahman <bappy.gm@gmail.com>
2019-11-09 00:16:07 +0600

    use mysql instead of sqlite3, added seed data

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ ruby '2.4.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
@@ -42,6 +42,8 @@ gem 'bcrypt','~> 3.1.7'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
+
+gem 'mysql2'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development?
+  puts ''
+  puts 'Runnning db/seed.rb'
+  Product.create(name: 'Signature Tehari', price: 120, status: 'Available')
+  Product.create(name: 'Chicken Tehari', price: 100, status: 'Available')
+  Product.create(name: 'Polao & Roast', price: 110, status: 'Available')
+  Product.create(name: 'Firni (Regular)', price: 130, status: 'Available')
+  Product.create(name: 'Firni (Large)', price: 150, status: 'Available')
+  puts 'Completed seed data'
+  puts ''
+else
+  puts "This is #{Rails.env} environment"
+  puts 'You can run seed data only in develpment environment.'
+end
