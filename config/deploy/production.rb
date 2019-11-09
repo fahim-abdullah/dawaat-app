8447ab2c452b6001be2d6c9860b038e116927030
Mahfuzur Rahman <bappy.gm@gmail.com>
2019-11-09 13:52:29 +0600

    installed capistrano gem

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '204.48.17.139', user: 'deployer', roles: %w{app db web}
+server '134.209.151.87', user: 'deployer', roles: %w{app db web}
 
 # role-based syntax
 # ==================
