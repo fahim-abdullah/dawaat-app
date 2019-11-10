da5791f5413e32e39e1f7001116e076b01f9b07f
Mahfuzur Rahman <bappy.gm@gmail.com>
2019-11-10 12:40:55 +0600

    updated asset_host for development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
+  config.action_controller.asset_host = 'http://dawaat.co'
+
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
