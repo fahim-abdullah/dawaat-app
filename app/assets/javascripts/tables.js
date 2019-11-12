94753633052e84eaa13f6513e98c01145607c420
fahim-abdullah <fahimabdullahh@gmail.com>
2019-11-12 23:23:38 +0800

    navbar-users, bg-cover and datatables change

--- app/assets/javascripts/tables.js
+++ app/assets/javascripts/tables.js
@@ -2,4 +2,8 @@ $( document ).on('turbolinks:load', function() {
   window.all_orders_table = $('#all-orders-table').DataTable({
   	"order": [[0, "desc" ]]
   });
+  window.ongoing_orders_table = $('#ongoing-orders-table').DataTable();
+  window.all_products_table = $('#all-products-table').DataTable();
+  window.all_users_table = $('#all-users-table').DataTable();
+  window.my_orders_table = $('#my-orders-table').DataTable();
 } );
