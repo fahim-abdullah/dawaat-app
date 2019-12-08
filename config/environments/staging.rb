f9a5155135110bf256b821a3fa10850f26d80f06
Mahfuzur Rahman <bappy.gm@gmail.com>
2019-12-08 12:37:48 +0600

    updated asset_host for staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,4 +3,5 @@ require File.expand_path('../production.rb', __FILE__)
  
 Rails.application.configure do
   # Here override any defaults
+  config.action_controller.asset_host = 'demo.dawaat.co'
 end
