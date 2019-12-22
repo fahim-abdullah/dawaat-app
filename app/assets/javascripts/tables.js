a459bc2adb27d8e839c78602c9b04f919ee1ce20
Mahfuzur Rahman <bappy.gm@gmail.com>
2019-12-22 22:28:04 +0600

    Bug fix: datatable turbolink - browser back button issue

--- app/assets/javascripts/tables.js
+++ app/assets/javascripts/tables.js
@@ -1,10 +1,10 @@
 $( document ).on('turbolinks:load', function() {
-
-  
-
   window.all_orders_table = $('#all-orders-table').DataTable({
   	retrieve: true,
 	"order": [[0,'desc']]
   });
+} );
 
+$( document ).on('turbolinks:before-cache', function() {
+  window.all_orders_table.destroy();
 } );
